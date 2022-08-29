6102a0945da65c06df9064e653f4d68ce7ba99d5
Dominik Kormann <dominikormann@outlook.com>
2022-08-29 02:57:40 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
+from multiprocessing.util import sub_debug
+from turtle import back
 from xml.dom.expatbuilder import theDOMImplementation
 import numpy as np
 
+from solver import solve as backend_solve
 
 
-# def set_field(data):
-#     for i in range(len(data)):
-#         if data[i]:
-#             fix_tile(i,data[i]) 
+def solve(sudoku):
+    """pass soduko as string list or array"""
+    data = []
+    if type(sudoku) == str:
+        for char in sudoku:
+            try:
+                next_int = int(char)
+                data.append(next_int)
+            except:
+                pass
+        assert len(data) == 81, 'cannot read data. sudoku needs to be 9x9'
+        data = np.array(data)
+    elif type(sudoku) == list:
+
+        data = np.array(sudoku)
+
+    elif type(sudoku) == np.ndarray:
+        data = sudoku.flatten()
+    else:
+        raise ValueError("cannot interpret given data. data needs to be list array or string")
+
+    result = backend_solve(data)
+    print_field(result)
+
+
 
 
 def print_field(field):
@@ -25,79 +49,6 @@ def print_field(field):
             print()
         print('+-------+-------+-------+')
 
-
-
-
-
-# def find_next_tryout(field = field):
-#     possibles = np.where(field ==0)[0]
-#     possible_options = options[possibles]
-#     argmin = possible_options.sum(1).argmin()
-#     return possibles[argmin]
-
-# def fix_tile(tile,value):
-#     for i in range(9):
-#         options[tile][i] = False
-#     options[tile][value-1] = True
-#     set_tile(tile,value)
-
-#     def set_tile(tile,value):
-#         # print(f"setting_tile {tile} {value}")
-#         assert field[tile] == 0
-#         field[tile] = value
-#         history.append((set_tile,tile,value))
-#         res = True
-#         for related in relations[tile]:
-
-#             if not remove_option(related,value):
-#                 # traceback()
-#                 res = False
-#         return res 
-
-
-
-# def tryout(field = field):
-#     nexttry = find_next_tryout(field)
-#     try:
-#         new_value = np.where(options[nexttry])[0][0]
-#     except:
-#         traceback()
-
-#     if not try_tile(nexttry,new_value+1):
-#         traceback()
-
-# def try_tile(field,value):
-#     history.append((try_tile,field,value))
-#     return set_tile(field,value)
-
-# def revert(action):
-#     action,tile,value = action
-#     if action == remove_option:
-#         options[tile][value] = True
-#         field[tile] = 0
-#     elif action == set_tile:
-#         field[tile] = 0
-
-
-# def traceback():
-#     last_action = history.pop()
-#     while last_action [0] != try_tile:
-#         revert(last_action)
-#         try:
-#             last_action = history.pop()
-#         except:
-#             print("no solution possible")
-#             raise ValueError
-#     action, tile,value = last_action
-#     # options[tile][value-1] = False
-#     if not remove_option(tile,value):
-#         traceback()
-
-# def solve():
-#     while (field == 0).sum()>0:
-#         try:
-#             tryout()
-#         except:
-#             break
-#     print_field()
-
+if __name__ == "__main__":
+    intake = input("input sudoku data ")
+    solve(intake)
